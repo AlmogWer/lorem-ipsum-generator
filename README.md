90191a7f70ff235b7aecc2ac3cdb135b7ea5d97b
Almog Wertzberger <Almogish@gmail.com>
2021-09-13 11:46:31 +0300

    adding functionality

---
+++ README.md
@@ -0,0 +1,40 @@
+<h1 align="center">Lorem-Ipsum-Generator-Hebrew</h1>
+
+## Project Description
+
+React Project to create a Lorem Ipsum generator in hebrew including input validations to show maximum 50 P
+
+## Built with
+
+- HTML
+- CSS
+- React
+
+# npm-start
+
+[npm start](https://docs.npmjs.com/cli/v7/commands/npm-start)
+
+```
+npm install
+npm install react-icons --save
+```
+
+```
+npm-start -g
+
+```
+
+## Links
+
+- [Repo](https://github.com/AlmogWer/lorem-ipsum-generator-hebrew "Tabs Repo")
+- [Live](https://almogwer-lorem-ipsum-generator.netlify.app/ "Live View")
+
+## Screenshots
+
+![](img/Capture.PNG "Home Page")
+
+## Author
+
+- [Almogwer](https://github.com/almogwer)
+- [Mail](mailto:Almogish@gmail.com?Subject=Hi% "Hi!")
+- [Linkedin](https://www.linkedin.com/in/almogwertzberger/)
--- src/App.js
+++ src/App.js
@@ -3,34 +3,58 @@ import data from "./data";
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello world");
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+      setCount("1");
+    }
+    if (count > 8) {
+      amount = data.length;
+      setCount(data.length);
+    }
+    setText(data.slice(0, amount));
   };
   return (
     <section className="section-center">
-      <h3>Tired of boring lorem ipsum?</h3>
-      <form className="lorem-form" onSubmit={handleSubmit}>
-        <label htmlFor="amount">paragraphs:</label>
-        <input
-          type="number"
-          name="amount"
-          id="amount"
-          value={count}
-          onChange={(e) => setCount(e.target.value)}
-        />
-        <button type="submit" className="btn">
-          Generate
-        </button>
-      </form>
-      <article className="lorem-text">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, enim!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, enim!
-        </p>
-      </article>
+      <div className="container">
+        <h3>מחולל לורם-איפסום</h3>
+
+        <form className="lorem-form" onSubmit={handleSubmit}>
+          <button type="submit" className="btn toRight">
+            חולל!
+          </button>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            value={count}
+            onChange={(e) => {
+              const val =
+                e.target.value >= data.length
+                  ? data.length
+                  : e.target.value < 1
+                  ? 1
+                  : e.target.value;
+              setCount(val);
+            }}
+          />
+          <label htmlFor="amount" className="toRight">
+            פסקאות:
+          </label>
+        </form>
+        <article className="lorem-text">
+          {text.map((item, index) => {
+            return (
+              <p key={index} className="toRight">
+                {item}
+              </p>
+            );
+          })}
+        </article>
+      </div>
     </section>
   );
 }
